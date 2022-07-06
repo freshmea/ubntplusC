371fab8e19723683e2aaebd3d490c41f10619d9d
choisugil <freshmea@naver.com>
2022-07-06 14:17:13 +0900

    celcius

---
+++ fahr2celsius.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int fahr;
+	int celsius;
+	
+	fahr = 100;
+	
+	selsius = 5/9*(fahr -32);
+	
+	printf("fahrenheit temp : ??? ---> celsius temp: ???\n");
+	
+	return 0;
+}
