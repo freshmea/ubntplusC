8fe077f84839259a2c691a738c53108957036f36
choisugil <freshmea@naver.com>
2022-07-07 12:49:37 +0900

    incrementor

--- study3/circle.c
+++ study3/circle.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#define PI 3.141592
 
 int main()
 {
@@ -8,7 +9,7 @@ int main()
 
 	printf("반지름은 (cm):");
 	scanf("%d", &radius);
-	area = radius *radius *3.141592;
+	area = radius *radius *PI;
 	printf("반지름이 %dcm인 원의 넓이는 %.2fcm^2입니다.\n", radius, area);
 
 	return 0;
