c2e7b10ae32202f3a03d4148a709f79755174d10
freshmea <freshmea@naver.com>
2022-07-13 14:31:47 +0900

    pwm

--- study7/test/test.ino
+++ study7/test/test.ino
@@ -1,15 +1,12 @@
+const int LED = 13;
+
 void setup(){
-  Serial.begin(115200);
-
-  Serial.println(78, DEC);
-  Serial.println(78, HEX);
-  Serial.println(78, BIN);
-
-  Serial.println(1.23456, 0);
-  Serial.println(1.23456, 2);
-  Serial.println(1.23456, 4);
+  pinMode(LED, OUTPUT);
 }
 
 void loop(){
-  
+  digitalWrite(LED, HIGH);
+  delay(1);
+  digitalWrite(LED, LOW);
+  delay(9);
 }
