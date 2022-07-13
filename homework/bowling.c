c77a68272b68a867cbe5ced3c563296ad14faef9
choisugil <freshmea@naver.com>
2022-07-13 14:05:46 +0900

    bawling

--- homework/bowling.c
+++ homework/bowling.c
@@ -7,55 +7,41 @@
 
 int main(void)
 {
-	int score1 = 0;
-	int score2 = 0;
+	int score[21] = {0};
+	int iscore = 0;
 	int frame = 1;
 	int cast = 1;
+	int try = 0;
 	int tscore = 0;
 	int strikeOn = 0;
 	int spareOn = 0;
 
 	for(;;){
-		score1= 0;
-		score2= 0;
+		iscore = 0;
 		printf("%d frame %d cast : ", frame, cast);
-		
-		if (cast == 1)
-			scanf("%d", &score1);
-		else
-			scanf("%d", &score2);
 
-		//보너스 계산 
-		if (strikeOn)
-			tscore += score1;
-		if (spareOn)
-			tscore += score1;
-		
-		if ( cast == 2 || score1 == 10){
-			//스타아리크 온이고 2캐스트일때 계산
-			if (strikeOn)
-				tscore += score2;
+		scanf("%d", &iscore);
+		printf("%d\n", try);
+		score[try] = iscore;
 
-			strikeOn = 0;
-			spareOn = 0;
+		++try;
+		if (iscore == 10 && cast != 2)
+			++try;
 
-			//스트라이크 와 스페어 처리 
-			if(score1 == 10)
-				strikeOn = 1;
-			else if((score1+score2) == 10)
-				spareOn = 1;
-
-			tscore += score1+score2;
+		tscore += iscore;
+		if ( cast == 2 || iscore == 10){
 
 			printf("\nframe: %d score: %d\n", frame, tscore);
 
 			++frame;
 			cast = 1;	
 		}else {
-			tscore += score1;
 			++cast;	
 		}
+
 		if( frame == 11){
+			for(int i=0;i<21;++i)
+				printf("%d", score[try]);
 			break;
 		}
 		
