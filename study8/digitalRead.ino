15234902f784f66d21ac37548b981de049b22b10
freshmea <freshmea@naver.com>
2022-07-14 15:25:01 +0900

    digital Read Write

--- study8/digitalRead.ino
+++ study8/digitalRead.ino
@@ -1,14 +1,11 @@
-const int digitalPin = 4;
-const int LED = 2;
+const int digitalPin = 2;
 
 void setup(){
   Serial.begin(9600);
   pinMode(digitalPin, INPUT);
-  pinMode(LED, OUTPUT);
 }
 
 void loop(){
   int digitalValue = digitalRead(digitalPin);
-  digitalWrite(LED, digitalValue);
   Serial.println(digitalValue);
 }
