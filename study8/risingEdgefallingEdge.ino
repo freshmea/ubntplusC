2011a4df4131a0a9c10a6e6657cd602719df0d82
freshmea <freshmea@naver.com>
2022-07-14 16:39:15 +0900

    pull up pull down button chattering rising edge falling edge

--- study8/risingEdgefallingEdge.ino
+++ study8/risingEdgefallingEdge.ino
@@ -16,8 +16,19 @@ void loop(){
   button_curr_state = buttonInput;
   if (button_curr_state == HIGH && button_prev_input == LOW){
     Serial.println("rising EDGE");
+    if(led_state){
+      led_state=LOW;
+    } else {
+      led_state=HIGH;
+    }
+    // 채터링 방지
+    delay(50);
   } else if (button_curr_state == LOW && button_prev_input == HIGH){
     Serial.println("falling EDGE");
+    // 채터링 방지
+    delay(50);
   }
   button_prev_input = button_curr_state;
+
+  digitalWrite(LED, led_state);
 }
