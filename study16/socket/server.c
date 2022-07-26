4fe1a039bc712917f893c30680718e692b86f4b4
choisugil <freshmea@naver.com>
2022-07-26 11:01:12 +0900

    socket coding

--- study16/socket/server.c
+++ study16/socket/server.c
@@ -1,14 +1,39 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+
+??? for_client_read_write( dataSocket)
+{
+	for (;;){
+	//read() / write()
+		if(end??) break;
+	}
+	close(dataSocket);
+}
 
 int main(void)
 {
-	socker();
+	socket();		//open()
+
+	int fd 		// file descriptor 파일 기술자.
+	fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (fd == -1){
+		fprintf(stderr, "cna't open socket()");
+		exit(1); 
+	}
 	bind();
 	listen();
-	accept();
-
-	rea()/write();
-
+	for(;;){
+		accept();
+		read()/write();
+		// create thread for read() / write();
+	}
+	
+	
+	
 	close();
 	return 0;
-	
+
 }
