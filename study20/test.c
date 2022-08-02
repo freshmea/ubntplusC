bdddbc127374f76f2d6153452f18eaf3a8bd7329
freshmea <freshmea@naver.com>
2022-08-02 15:43:57 +0900

    struct, string

--- study20/test.c
+++ study20/test.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
 
+struct abc{
+	char Array1[10];
+	char Array2[20];
+	char Array3[30];
+	char Array4[40];
+};
+
+void checksize(struct abc (*abc1))
+{
+	printf("%ld\n", sizeof(abc1->Array1));
+	printf("%ld\n", sizeof(abc1->Array2));
+	printf("%ld\n", sizeof(abc1->Array3));
+	printf("%ld\n", sizeof(*abc1));
+}
+
+void checksize2(char *Array)
+{
+	printf("%ld\n", sizeof(Array));
+}
+
 int main(void)
 {
 	// char str[5];
@@ -15,10 +35,18 @@ int main(void)
 	// const int *pa ;
 	// *pa = &a;
 	char Array[10]="hi";
-	printf("%ld\n", sizeof(Array));
+
+	struct abc abc1;
+	printf("%ld\n", sizeof(Array)/sizeof(Array[0]));
+	printf("%ld\n", sizeof(abc1.Array2)/sizeof(abc1.Array2[0]));
+
+	checksize(&abc1);
+	checksize2(Array);
+
 	char *pb = Array;
 	printf("%ld\n", sizeof(char));
 	printf("%ld\n", sizeof(pb));
+	checksize2(pb);
 	const int *pa =&a;
 	printf("%d", *pa);
 
---
+++ study21/StringNonVariable.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("apple이 저장된 시작 주소값 : %p\n", "apple");
+	printf("첫 번째 문자  : %f\n", (float)*("apple"));
+
+	// int str[] = {'a','p','p','l','e'};
+	char str[] = "apple is delicious";
+	printf("char size : %ld\n", sizeof(char));
+	printf("int size : %ld\n", sizeof(int));
+	printf("float size : %ld\n", sizeof(float));
+	printf("변수 : %s\n", str);
+	printf("변수 : %s\n", &str[0]);	
+	printf("변수 주소값 : %p\n", str);
+	printf("변수 주소값 : %p\n", str+1);
+	printf("변수 주소값 : %p\n", str+2);
+	printf("변수 주소값 : %p\n", str+3);
+	printf("변수 주소값 : %d\n", str[0]);
+	float *str2 = (float*)str;	// 자료를 플로트형으로 변환
+	printf("변수 주소값 : %p\n", str2);
+	printf("변수 주소값 : %p\n", str2+1);
+	printf("변수 주소값 : %p\n", str2+2);
+	printf("변수 주소값 : %p\n", str2+3);
+	printf("변수 주소값 : %f\n", str2[0]);
+	printf("변수 주소값 : %f\n", str2[1]);
+	printf("변수 주소값 : %f\n", str2[2]);
+
+	// printf("변수 주소값 : %f\n", (float)str);
+}
