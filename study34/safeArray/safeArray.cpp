26be8b56b93316c630123c87ca7cd0feb8978908
freshmea <freshmea@naver.com>
2022-08-22 20:12:31 +0900

    slight upgrade

--- study34/safeArray/safeArray.cpp
+++ study34/safeArray/safeArray.cpp
@@ -2,40 +2,10 @@
 #include <cassert>
 
 SafeArray::SafeArray(int size)
-: Array(size)
-{
-
-}
+: Array(size){}
 
 SafeArray::SafeArray(const int *pArr, int size)
-:Array(pArr, size)
-{
-
-}
-/*
-SafeArray::SafeArray(const SafeArray& rhs)
-: Array( (Array)rhs)		//타입 캐스팅을 하면 자식과 관련된 정보는 슬라이싱 된다. 
-{
-
-}
-
-SafeArray::~SafeArray()
-{
-
-}
-
-SafeArray& SafeArray::operator=(const SafeArray& rhs)
-{
-	return this->Array::operator=((Array)rhs);
-	// return (Array)(*this) = (Array)rhs;
-}
-*/			// 컴파일러에서 자동으로 만들어지는 코드 .
-
-
-// bool SafeArray::operator==(const SafeArray& rhs) const
-// {
-// 	return this->Array::operator==( (Array)rhs);
-// }			// 부모 클래스와 같으므로 생략 해도 똑같이 작동. 
+:Array(pArr, size){}
 
 int& SafeArray::operator[](int index)
 {
